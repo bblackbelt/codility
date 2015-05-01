e7e0facc12b2862f3948bd351e0d9b0c8c512d4e
blackbelt <emanuele.g@gmail.com>
2015-05-01 19:28:28 +0200

    leader and stack&queue package

--- src/com/blackbelt/codility/leader/EquiLeader.java
+++ src/com/blackbelt/codility/leader/EquiLeader.java
@@ -2,6 +2,92 @@ package com.blackbelt.codility.leader;
 
 /**
  * Created by emanuele on 01.05.15.
+ * A non-empty zero-indexed array A consisting of N integers is given.
+ * <p/>
+ * The leader of this array is the value that occurs in more than half of the elements of A.
+ * <p/>
+ * An equi leader is an index S such that 0 ≤ S < N − 1 and two sequences A[0], A[1], ..., A[S] and A[S + 1], A[S + 2], ..., A[N − 1] have leaders of the same value.
+ * <p/>
+ * For example, given array A such that:
+ * <p/>
+ * A[0] = 4
+ * A[1] = 3
+ * A[2] = 4
+ * A[3] = 4
+ * A[4] = 4
+ * A[5] = 2
+ * we can find two equi leaders:
+ * <p/>
+ * 0, because sequences: (4) and (3, 4, 4, 4, 2) have the same leader, whose value is 4.
+ * 2, because sequences: (4, 3, 4) and (4, 4, 2) have the same leader, whose value is 4.
+ * The goal is to count the number of equi leaders.
+ * <p/>
+ * Write a function:
+ * <p/>
+ * int solution(int A[], int N);
+ * <p/>
+ * that, given a non-empty zero-indexed array A consisting of N integers, returns the number of equi leaders.
+ * <p/>
+ * For example, given:
+ * <p/>
+ * A[0] = 4
+ * A[1] = 3
+ * A[2] = 4
+ * A[3] = 4
+ * A[4] = 4
+ * A[5] = 2
+ * the function should return 2, as explained above.
+ * <p/>
+ * Assume that:
+ * <p/>
+ * N is an integer within the range [1..100,000];
+ * each element of array A is an integer within the range [−1,000,000,000..1,000,000,000].
+ * Complexity:
+ * <p/>
+ * expected worst-case time complexity is O(N);
+ * expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
  */
 public class EquiLeader {
+    public int solution(int[] A) {
+        int value = 0;
+        int counter = 0;
+        for (int x : A) {
+            if (counter == 0) {
+                ++counter;
+                value = x;
+            } else {
+                if (x == value) {
+                    ++counter;
+                } else {
+                    --counter;
+                }
+            }
+        }
+        int candidate = -1;
+        if (counter > 0) {
+            candidate = value;
+        }
+        int total = 0;
+        for (int x : A) {
+            if (x == candidate) {
+                ++total;
+            }
+        }
+        if (total < A.length / 2) {
+            return 0;
+        }
+        int leftLeader = 0;
+        counter = 0;
+        for (int i = 0; i < A.length; i++) {
+            value = A[i];
+            if (value == candidate) {
+                ++leftLeader;
+            }
+            int rightLeader = total - leftLeader;
+            if (leftLeader > (i + 1) / 2 && rightLeader > (A.length - i - 1) / 2) {
+                ++counter;
+            }
+        }
+        return counter;
+    }
 }
--- src/com/blackbelt/codility/stacksandqueues/Nesting.java
+++ src/com/blackbelt/codility/stacksandqueues/Nesting.java
@@ -2,6 +2,46 @@ package com.blackbelt.codility.stacksandqueues;
 
 /**
  * Created by emanuele on 12.04.15.
+ * <p/>
+ * A string S consisting of N characters is called properly nested if:
+ * <p/>
+ * S is empty;
+ * S has the form "(U)" where U is a properly nested string;
+ * S has the form "VW" where V and W are properly nested strings.
+ * For example, string "(()(())())" is properly nested but string "())" isn't.
+ * <p/>
+ * Write a function:
+ * <p/>
+ * class Solution { public int solution(String S); }
+ * <p/>
+ * that, given a string S consisting of N characters, returns 1 if string S is properly nested and 0 otherwise.
+ * <p/>
+ * For example, given S = "(()(())())", the function should return 1 and given S = "())", the function should return 0, as explained above.
+ * <p/>
+ * Assume that:
+ * <p/>
+ * N is an integer within the range [0..1,000,000];
+ * string S consists only of the characters "(" and/or ")".
+ * Complexity:
+ * <p/>
+ * expected worst-case time complexity is O(N);
+ * expected worst-case space complexity is O(1) (not counting the storage required for input arguments).
  */
 public class Nesting {
+    public int solution(String S) {
+        int openCounter = 0;
+        char[] brackets = S.toCharArray();
+        for (int i = 0; i < brackets.length; i++) {
+            char current = brackets[i];
+            if (current == ')') {
+                if (openCounter <= 0) {
+                    return 0;
+                }
+                --openCounter;
+                continue;
+            }
+            ++openCounter;
+        }
+        return openCounter == 0 ? 1 : 0;
+    }
 }
